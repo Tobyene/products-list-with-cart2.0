4da1d2b53f8b6f9f49885ef526a51e012ddb1a87
Vinícius de Oliveira Miranda <110639789+Tobyene@users.noreply.github.com>
2024-09-20 18:25:50 -0300

    Update product.ts

--- src/entities/product.ts
+++ src/entities/product.ts
@@ -1,21 +1,24 @@
 import { v4 as randomUUID } from "uuid";
 
+// Product.ts
 export class Product {
-    private _id: string = randomUUID();
-    private _imageUrl: string;
-    private _category: string;
-    private _productName: string;
-    private _price: number;
+    private imageUrl: string;
+    private category: string;
+    private productName: string;
+    private price: number;
 
-    constructor ( 
-        imageUrl: string,
-        category: string,
-        productName: string,
-        price: number
-    ){
-        this._imageUrl = imageUrl;
-        this._category = category;
-        this._productName = productName;
-        this._price = price;
+    constructor(imageUrl: string, category: string, productName: string, price: number) {
+        this.imageUrl = imageUrl;
+        this.category = category;
+        this.productName = productName;
+        this.price = price;
+    }
+
+    getPrice(): number {
+        return this.price;
+    }
+
+    getProductName(): string {
+        return this.productName;
     }
 }
